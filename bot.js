2f3badacf15ffa90435122768ab8ad0135947044
juice <moses@thevinsons.com>
2019-02-15 23:15:21 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,73 @@
+var timerBoi = require('./my.json');
+const fs = require('fs');
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
+  console.log('I am ready!');
+});var timerBoi = require('./my.json');
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
 
-    console.log('I am ready!');
- 
-
+client.on('ready', () => {
+	console.log('I am ready!');
 });
 
- 
+client.on("message", (message) => {
+	if (message.content.includes('`.grab`')) {
+		message.channel.send('.grab');
+	}
+});
 
-client.on('message', message => {
+var farm = setInterval(function () {
+	var goalTime = timerBoi.data + 21600000;
+	var currentTime = new Date().getTime();
+	if (currentTime >= goalTime) {
+		client.channels.get("402919650985246743").send('.harvest cannabis');
+		storeTheBoi();
+	}
+}, 6000);
 
-    if (message.content === 'Someone just dropped their wallet in this channel! Hurry and pick it up with `.grab` before someone else gets it!') {
 
-       message.channel.send('.grab');
+function storeTheBoi() {
+	var n = new Date().getTime();
+	fs.writeFile('./my.json',
+		"{\"data\": " + n + "}",
+		function (err) {
+			if (err) {
+				console.error('Crap happens');
+			}
+		}
+	)
+}
 
-       }
- if(message.content === '.harvest cannabis'){
-  var hare = true;
-
+client.on("message", (message) =>{
+  if(message.content.includes('`.grab`')){
+    message.channel.send('.grab');
   }
-
 });
 
-client.on('message', message => {
-    if(message.content === 'harvezt'){
-console.log('sending harvest in six hours and two minutes');
-        var interval = setInterval (function () {
-        message.channel.send('.harvest cannabis')
-            .catch(console.error);
-      }, 21720000); 
+var farm = setInterval(function(){
+var goalTime=timerBoi.data+21600000;
+var currentTime=new Date().getTime();
+if(currentTime>=goalTime){
+  client.channels.get("402919650985246743").send('.harvest cannabis');
+  storeTheBoi();
+} 
+},6000);
+
+
+function storeTheBoi(){
+var n = new Date().getTime();
+fs.writeFile('./my.json',
+    "{\"data\": "+n+"}",
+    function (err) {
+        if (err) {
+            console.error('Crap happens');
+        }
+    }
+)
 }
-    });
-
-function wait(ms){
-var startTime = getTime();
-var currentTime = getTime();
-while(currentTime - startTime < ms){
-currentTime = getTime();
-}
-}
- 
-
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
