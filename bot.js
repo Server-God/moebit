b1cfd4cf2f1e03f56224c5ae2cbc542c7ad04385
juice <moses@thevinsons.com>
2019-02-16 15:18:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,9 @@ client.on('ready', () => {
 });
 
 client.on("message", (message) =>{
-  if (message.content === "listemojis") {
-  const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
-  message.channel.send(emojiList);
+  if(message.content === "gay wumpus") {
+   const ayy = client.emojis.find(emoji => emoji.name === "wumpusRainbow");
+   message.reply(`${wumpusRainbow} LMAO`);
 }
   if(message.content.includes('`.grab`')){
     message.channel.send('.grab');
