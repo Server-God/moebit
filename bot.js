317d2b95bf28b353ca977a774af5e066e992e9df
juice <moses@thevinsons.com>
2019-02-16 07:59:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ var currentTime=new Date().getTime();
 if(currentTime >= goalTime){
   client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
   storeTheBoi();
-  goalTime=(new Date().getTime())+21720000;
+  goalTime=(new Date().getTime()+21720000);
 } else {currentTime=new Date().getTime();}
 },6000);
 
@@ -46,7 +46,7 @@ function storeABoi(datA){
 }
 
 function storeTheBoi(){
-var n = (new Date().getTime())+21720000;
+var n = (new Date().getTime()+21720000);
 client.channels.get(process.env.FARM_CHANNELID).send(new Date().getTime());
 client.channels.get(process.env.FARM_CHANNELID).send(n);
 storeABoi(n);
