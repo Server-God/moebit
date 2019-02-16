21157c706ea0c37481903fae99fe36952c926481
juice <moses@thevinsons.com>
2019-02-16 08:17:47 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,35 @@
-var timerBoi = require('./my.json');
-const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-var n;
+var timeLeft = 0;
+var n = true;
 
 client.on('ready', () => {
   console.log('I am ready!');
-  console.log(timerBoi.data);
-  console.log(new Date().getTime());
+  client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
 });
 
 client.on("message", (message) =>{
+  
   if(message.content.includes('`.grab`')){
     message.channel.send('.grab');
   }
-  if(message.content.includes('Mrharvezt')){
+  if(message.content.includes('before you can harvest your cannabis crops again')){
     var array = message.content.split(" ");
-    var sotre = new Date().getTime() + Number(array[1]);
-    storeABoi(sotre);
-    var timerBoi = require('./my.json');
+    var hour = array[4];
+    var minutes = array [7];
+    timeLeft = new Date().getTime() + (hour * 3600000) + (minutes * 60000);
     message.channel.send(timerBoi.data);
     message.channel.send(sotre);
   }
 });
 
 var farm = setInterval(function(){
-var timerBoi = require('./my.json');
-var goalTime=timerBoi.data;
+var goalTime = timeLeft;
 var currentTime=new Date().getTime();
 if(currentTime >= goalTime){
-  //client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
-  storeTheBoi();
+  client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
   goalTime=(new Date().getTime()+21720000);
   currentTime=new Date().getTime();
 }
 },6000);
-
-function storeABoi(datA){
-  fs.writeFile('./my.json',
-    "{\"data\": "+datA+"}",
-    function (err) {
-        if (err) {
-            console.error('Crap happens');
-        }
-    }
-)
-}
-
-function storeTheBoi(){
-var n = (new Date().getTime()+21720000);
-client.channels.get(process.env.FARM_CHANNELID).send(new Date().getTime());
-client.channels.get(process.env.FARM_CHANNELID).send(n);
-storeABoi(n);
-}
