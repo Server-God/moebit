b5915b27b28c70df8882c5e971f39e51f4fcc290
juice <moses@thevinsons.com>
2019-02-22 16:45:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
+const cmd = '.harvest cannabis';
+const botIntervTime = 21720000;
+const triggerMessage = 'before you can harvest your cannabis crops again';
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-var timeLeft = Time() + 21720000;
+var timeLeft = Time() + botIntervTime;
 
 function Time(){ 
   var n = new Date();
@@ -10,19 +14,21 @@ function Time(){
 
 client.on('ready', () => {
   console.log('I am ready!');
-  client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
+  client.channels.get(process.env.FARM_CHANNELID).send(cmd);
 });
 
 client.on("message", (message) =>{
   if(message.content.includes('`.grab`')){
     message.channel.send('.grab');
   }
-  if(message.content.includes('before you can harvest your cannabis crops again')){
+  if(message.content.includes(triggerMessage)){
     if (message.channel.id === process.env.FARM_CHANNELID){
     var array = message.content.split(" ");
     var hour = Number(array[4]);
     var minutes = Number(array [7]);
     timeLeft = Time() + (hour * 3600000) + (minutes * 60000) + 120000;
+    console.log(mstohour(timeLeft-Time()));
+    message.channel.send(mstohour(timeLeft-Time()));
     message.channel.send(timeLeft);
     message.channel.send(Time());
   } 
@@ -33,8 +39,8 @@ var farm = setInterval(function(){
 var goalTime = timeLeft;
 var currentTime=Time();
 if(currentTime >= goalTime){
-  client.channels.get(process.env.FARM_CHANNELID).send('.harvest cannabis');
-  timeLeft=(Time()+21720000);
+  client.channels.get(process.env.FARM_CHANNELID).send(cmd);
+  timeLeft=(Time()+botIntervTime);
   goalTime=timeLeft;
   currentTime=Time();
 }
