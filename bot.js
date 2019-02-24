9832f911bb69395b3486daad124581d4f72ba93a
juice <moses@thevinsons.com>
2019-02-24 06:17:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ function Time(){
 function mstohour(ms){
   var text = "";
   var hours = Math.floor(ms / 3600000);
-  var minutes = Math.floor((ms - (hours * 3600000)) / 1000);
+  var minutes = Math.floor((ms / 1000) - (hours * 3600));
   return text + hours + " hours and " + minutes + " minutes"
 }
 
