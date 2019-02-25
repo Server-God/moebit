6f2433ce7786bb529bc222dbde1e3aefcfe469c0
juice <moses@thevinsons.com>
2019-02-25 15:32:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-
+var spamShoot = false;
+var spamInter = 6000;
+var spamMsg = "";
+var spamChannel = 0;
 client.on('ready', () => {
   console.log('I am ready!');
 });
@@ -13,4 +16,19 @@ client.on("message", (message) =>{
 if(message.member.roles.has(process.env.SUS_ROLE)){ 
 	message.react(process.env.SUS_EMOJI);
 }
+	if (message.content.includes("`activate"){
+	    //trigger | interval in seconds | command
+	let array = message.content.split(" | ");
+	spamChannel = message.channel.id;
+	spamMsg = array[2];
+	spamInter = Number(array[1]) * 1000;
+	message.channel.send(message.member);
+	    }
+
 });
+
+var spam = setInterval(function(){
+	if (spamShoot = true){
+		client.channels.get(spamChannel).send(spamMsg);
+	}
+}, spamInterv);
