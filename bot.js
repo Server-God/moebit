de1558f12eeec417815c35dd0f4b2a6aa9374d46
juice <moses@thevinsons.com>
2019-02-25 16:21:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
+var prefix = "~~";
 var text = "";
 client.on('ready', () => {
   console.log('I am ready!');
@@ -18,12 +19,12 @@ if(message.member.roles.has(process.env.SUS_ROLE)){
 });
 
 client.on("message", (message) =>{
-	if (message.content.includes("~~activate")){
+	if (message.content.includes(prefix+"activate")){
 	    //trigger | interval in seconds | command
 		let array = message.content.split(" ");
 		var spamInterv = Number(array[1]) * 1000;
 		var spamMsg = message.content.split(" ").slice(2).join(" ");
-		console.log(spamChannel + "\n" + spamMsg + "\n" + spamInterv);
+		console.log(spamMsg + "\n" + spamInterv);
 		
 		var spam = setInterval(function(){
 	message.channel.send(spamMsg);
