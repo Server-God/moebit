edabb0380c95125399e60d4c7f4eb7f9a19d5fec
juice <moses@thevinsons.com>
2019-03-16 15:25:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//j
 const config = require('./config.json');
 function random(low, high){
   return Math.floor(Math.random() * (high - low) + low)
