d2bb9b4ed3469674c6b67f92828ef9d96660c5bb
juice <moses@thevinsons.com>
2019-03-16 15:45:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ function random(low, high){
 }
 function gameBoi(){
   var n = random(0,config.games.length);
-  client.user.setPresence({ status: 'online', game: { name: 'GAME HERE' } });
+  client.user.setGame("ugh");
 }
 var spam;
 client.on('ready', () => {
