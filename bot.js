be46f95180c98b4f99eb886ec305a63d5100131e
juice <moses@thevinsons.com>
2019-03-16 16:13:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,90 @@
-const cmd = '.harvest opium';
-const botIntervTime = 72120000;
-const triggerMessage = 'before you can harvest your opium crops again';
-
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN);
-var timeLeft = Time() + botIntervTime;
-
-function Time(){ 
-  var n = new Date();
-  return n.getTime();
-}
-
-function mstohour(ms){
-  var text = "";
-  var hours = Math.floor(ms / 3600000);
-  var minutes = Math.floor((ms - (hours * 3600000))/60000);
-  return text + hours + " hours and " + minutes + " minutes"
-}
-
-client.on('ready', () => {
-  console.log('I am ready!');
-  client.channels.get(process.env.FARM_CHANNELID).send(cmd);
+const config = require("./config.json");
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.username} with the id of ${client.user.id}`);
+  client.user.setActivity("ugh");
 });
 
-client.on("message", (message) =>{
-  if(message.content.includes(triggerMessage)){
-    if (message.channel.id === process.env.FARM_CHANNELID){
-    var array = message.content.split(" ");
-    var hour = Number(array[4]);
-    var minutes = Number(array [7]);
-    var remainingTime = (hour * 3600000) + (minutes * 60000) + 120000;
-    timeLeft = Time() + remainingTime;
-    console.log(mstohour(remainingTime));
-    message.channel.send(mstohour(remainingTime));
-  } 
+client.on("message", async message => {
+  //anyone who's not a bot: below
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command == "ping"){
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+
+  //only cool cats below
+  function checkifCool(element){
+    return element == message.author.id;
+  }if (!config.advPpl.some(checkifCool)) return;
+
+  if (command == "yo"){
+    const m = await message.channel.send("Yo?");
+    m.edit(`Yo! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    console.log("yep.")
+  }
+  if (command == "logme") {
+    console.log(message);
+    message.channel.send("ugh. fine.")
   }
 });
 
-var farm = setInterval(function(){
-var goalTime = timeLeft;
-var currentTime=Time();
-if(currentTime >= goalTime){
-  client.channels.get(process.env.FARM_CHANNELID).send(cmd);
-  timeLeft=(Time()+botIntervTime);
-  goalTime=timeLeft;
-  currentTime=Time();
-}
-},6000);
+client.login(config.token);
