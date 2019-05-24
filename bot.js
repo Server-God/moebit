93f8bcb7b3a9a0eea0cba154124b85a493b4d06a
juice <moses@thevinsons.com>
2019-05-23 21:38:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ return output
 }
 
 client.on('ready', () => {
-client.user.setActivity("my code", { type: "WATCHING"})
+client.user.setActivity(prefix+"help for help", { type: "PLAYING"})
   console.log('I am ready!');
 });
 client.login("NTgxMzMzMjM1OTM4NjIzNTA4.XOdvkQ.Msa-vYNPEJCFQ-R1DOMqaQRBPA0");
