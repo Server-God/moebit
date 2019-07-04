f0fe76d5cea2dd7682d5864b566aabb57880e2c8
juice <moses@thevinsons.com>
2019-07-04 14:40:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,4 +56,9 @@ client.on("message", (message) =>{
 	    }, spamInterv);
 	};
 });
+
+Client.on('messageDelete', function(message, channel){
+  client.channels.get("596454885248204800").send(channel+"\n\n"+JSON.stringify(message));
+});
+
 client.login(process.env.BOT_TOKEN);
