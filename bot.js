f8cfda0279172d7c8172b4067bcc6572bb4dff33
juice <moses@thevinsons.com>
2019-11-10 18:06:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,50 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
-function random(low, high){
-  return Math.floor(Math.random() * (high - low) + low)
-}
+
+var myID = process.env.myID;
+var prefix = process.env.prefix;
+var token = process.env.BOT_TOKEN;
+
+client.on('ready', () => {
+  console.log('I am ready!');
+  console.log(cipher("e","mouse bois uniTe"));
+});
+
+client.on("message", (message) =>{
+  if(message.content.indexOf(prefix) !== 0) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  var command = args.shift().toLowerCase();
+	
+	if (command == "decrypt" || command == "encrypt") {
+		var simp=capCase(command);
+		var st=simp.f, title=simp.c;
+		var color = random(1,16777215);
+		var text = args.join(' '), result = cipher(st, text);
+		console.log(st+"\n"+text+"\n"+result);
+		
+		const embed = {
+  "title": title,
+  "description": "Your og text: \"**"+text+"\"**",
+  "color": color,
+  "author": {
+    "name": "Leslie the Useful Bot",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
+    {
+      "name": "🤔",
+      "value": "***"+result+"***"
+    }
+  ]
+};
+message.channel.send({ embed });
+		
+		}
+	
+});
+
+
 function cipher(status,string) {
 	//shift all letters by 13
 	//assign letters numbers according to alphabet ex (a is 1 j is 10)
@@ -34,88 +75,13 @@ function cipher(status,string) {
 	return output.toUpperCase()
 }
 
-console.log(cipher("e","mouse bois uniTe"))
-function gameBoi(){
-  var n = random(0,config.games.length-1);
-  client.user.setActivity(config.games[n]);
+function random(low, high){
+  return Math.floor(Math.random() * (high - low) + low)
 }
-var spam;
-client.on('ready', () => {
-  //gameBoi()
-  console.log('I am ready!');
-});
-//var games = setInterval(gameBoi,600000);
 
-client.on("message", (message) =>{
-  if(message.content.includes('`.grab`')){
-    message.channel.send('.grab');
-  }
-  if(message.content.indexOf(process.env.prefix) !== 0) return;
-  const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-/*if(message.author.id == process.env.SUS_ROLE){ 
-	message.react(process.env.SUS_EMOJI);
-}*/
-	//all these depend on me sending
-if (message.author.id !== process.env.myID) return;
-	if (command == "stop"){
-	message.delete();
-	clearInterval(spam);	
+function capCase(txt){
+	var temp = txt, h= temp.charAt(0), c= temp.slice(1);
+	return {'f': h, 'n': c, 'c': h.toUpperCase()+c}
 	}
-	/*if (command == "whoosh"){
-	const newGame = args.join(" ");
-	if (!newGame) return message.reply("yo, idiot you forgot what to set your game to");
-    	message.delete().catch(O_o=>{}); 
-	clearInterval(games);
-	client.user.setActivity(newGame);
-	games = setInterval(gameBoi,600000);
-	}
-	if (command == "set"){
-	const newGame = args.join(" ");
-	if (!newGame) return message.reply("yo, idiot you forgot what to set your game to");
-    	message.delete().catch(O_o=>{}); 
-        clearInterval(games);
-	client.user.setActivity(newGame);
-	}*/
-if (command = "decrypt") {
-var text = args.join(" ");
-message.channel.send(cipher("d", text));
-}
-if (command = "encrypt") {
-var text = args.join(" ");
-message.channel.send(cipher("e", text));
-}
-});
 
-client.on("message", (message) =>{
-	if(message.content.indexOf(process.env.prefix) !== 0) return;
-  const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-	if (command == "start"){
-		message.delete();
-		message.channel.send(spamMsg);
-	    //trigger interval in seconds command
-		var spamInter = args.shift();
-    spamInterv = (Number(spamInterv) * 1000) + 100;
-		spam = setInterval(function(){
-	message.channel.send(args);
-	    }, spamInterv);
-	};
-});
-
-client.on('messageDelete', async (message) => {
-  const logs = client.channels.get("596454885248204800");  
-  const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
-  let user = ""
-    if (entry.extra.channel.id === message.channel.id
-      && (entry.target.id === message.author.id)
-      && (entry.createdTimestamp > (Date.now() - 5000))
-      && (entry.extra.count >= 1)) {
-    user = entry.executor.username
-  } else { 
-    user = message.author.username
-  }
-  logs.send(`A message was deleted in ${message.channel.name} by ${user}`);
-})
-
-client.login(process.env.BOT_TOKEN);
+client.login(token);
