de4f22a4e05bc3914cd5020c673b4dd1ce598ffa
juice <moses@thevinsons.com>
2019-11-12 12:27:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ function capCase(txt){
 	}
 
 function mention(input){
-if (!(input.length==undefined))return false
+if (input==undefined)return false
 var temp= input.split(/ +/g);
 var b= temp[0]+temp[1];
 var e= temp[temp.length-1]
